a5daf361869935f6cf22ed69e12d39d052b24d1b
Constantin <648900-ConnyOnny@users.noreply.gitlab.com>
2021-07-05 02:28:14 +0200

    Finish and test qstr parsing
    Also do an autoformat

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,34 @@ use nom::character::complete::digit1;
 use nom::character::complete::multispace0;
 use nom::character::complete::multispace1;
 use nom::character::complete::not_line_ending;
-use nom::combinator::opt;
 use nom::combinator::map;
+use nom::combinator::opt;
 use nom::combinator::value;
-use nom::sequence::preceded;
 use nom::sequence::delimited;
+use nom::sequence::preceded;
 use nom::IResult;
+
+/// MaybeOwnedString
+#[derive(Debug, Eq, PartialEq)]
+enum MOS<'a> {
+    Reference(&'a str),
+    Owned(String),
+}
+
+impl<'a> AsRef<str> for MOS<'a> {
+    fn as_ref(&self) -> &str {
+        match self {
+            MOS::Reference(s) => s,
+            MOS::Owned(s) => &s,
+        }
+    }
+}
+
+impl<'a> std::fmt::Display for MOS<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        f.write_str(self.as_ref())
+    }
+}
 
 #[derive(Debug)]
 enum PestoCommand<'a> {
@@ -61,7 +83,6 @@ fn parse_mixed_numeral(s: &str) -> IResult<&str, Rational32> {
     }
 }
 
-
 fn take_until0_str(delim: &'static str) -> impl Fn(&str) -> IResult<&str, &str> {
     move |s| {
         let index = s.find(delim).unwrap_or_else(|| s.len());
@@ -71,7 +92,10 @@ fn take_until0_str(delim: &'static str) -> impl Fn(&str) -> IResult<&str, &str> 
 }
 
 fn parse_opt_unit(s: &str) -> IResult<&str, Option<&str>> {
-    alt((value(None, tag("_")), map(take_until0_str(" "), |s| Some(s))))(s)
+    alt((
+        value(None, tag("_")),
+        map(take_until0_str(" "), |s| Some(s)),
+    ))(s)
 }
 
 fn parse_ingredient(s: &str) -> IResult<&str, PestoCommand> {
@@ -81,7 +105,14 @@ fn parse_ingredient(s: &str) -> IResult<&str, PestoCommand> {
     let (s, unit) = parse_opt_unit(s)?;
     let (s, _) = multispace1(s)?;
     let (s, ingredient) = not_line_ending(s)?;
-    Ok((s, PestoCommand::Ingredient{amount, unit, ingredient}))
+    Ok((
+        s,
+        PestoCommand::Ingredient {
+            amount,
+            unit,
+            ingredient,
+        },
+    ))
 }
 
 fn parse_action(s: &str) -> IResult<&str, PestoCommand> {
@@ -107,7 +138,25 @@ fn parser(s: &str) -> IResult<&str, Vec<PestoCommand>> {
     Ok((s, result))
 }
 
-fn quoted_str_content(s: &str) -> IResult<&str, String> {
+fn my_split_at(s: &str, idx: usize) -> IResult<&str, MOS> {
+    let (inside, outside) = s.split_at(idx);
+    Ok((outside, MOS::Reference(inside)))
+}
+
+fn quoted_str_content(s: &str) -> IResult<&str, MOS> {
+    // try to do it no-copy
+    match (s.find('\\'), s.find('"')) {
+        (None, None) => return Ok(("", MOS::Reference(s))),
+        (None, Some(idx)) => return my_split_at(s, idx),
+        (Some(slash_idx), quote_idx_opt) => {
+            let quote_idx = quote_idx_opt.unwrap_or(s.len());
+            if quote_idx < slash_idx {
+                return my_split_at(s, quote_idx);
+            }
+        }
+    }
+    println!("Got: {}", s);
+    // have to copy the String because of escaping
     let mut escaped = false;
     let mut out = String::new();
     for (idx, c) in s.char_indices() {
@@ -125,35 +174,42 @@ fn quoted_str_content(s: &str) -> IResult<&str, String> {
                     out.push(c);
                     escaped = false;
                 } else {
-                    return Ok((&s[0..idx], out));
+                    let result = Ok((&s[idx..], MOS::Owned(out)));
+                    println!("Result: {:?}", result);
+                    return result;
                 }
             }
             _ => {
                 if escaped {
-                    tag("Invalid escape")(&s[idx-1..idx+1])?;
+                    tag("Invalid escape")(&s[idx - 1..idx + 1])?;
                     unreachable!()
                 }
                 out.push(c);
             }
         }
     }
-    Ok(("", s.into()))
+    let result = Ok(("", MOS::Owned(out)));
+    println!("Result (end): {:?}", result);
+    result
 }
 
-fn quoted_str(s: &str) -> IResult<&str, String> {
+fn quoted_str(s: &str) -> IResult<&str, MOS> {
     delimited(tag("\""), quoted_str_content, tag("\""))(s)
 }
 
-fn word(s: &str) -> IResult<&str, &str> {
-    let end_idx = s.char_indices().find(|(idx,c)| c.is_whitespace()).map(|(idx,c)| idx).unwrap_or(s.len());
-    if end_idx > 0 {
-        Ok((&s[end_idx..], &s[0..end_idx]))
-    } else {
-        tag("word would be empty")("")
+fn word(s: &str) -> IResult<&str, MOS> {
+    let end_idx = s
+        .char_indices()
+        .find(|(_idx, c)| c.is_whitespace())
+        .map(|(idx, _c)| idx)
+        .unwrap_or(s.len());
+    if end_idx == 0 {
+        tag("word would be empty")("")?;
     }
+    my_split_at(s, end_idx)
 }
 
-fn empty_word(s: &str) -> IResult<&str, &str> {
+fn empty_word(s: &str) -> IResult<&str, MOS> {
     let mut chars_it = s.chars();
     let success: bool;
     if let Some('_') = chars_it.next() {
@@ -165,11 +221,45 @@ fn empty_word(s: &str) -> IResult<&str, &str> {
     } else {
         success = false
     }
-    if success {
-        Ok((&s[1..], ""))
-    } else {
-        tag("not empty word")("")
+    if !success {
+        tag("not empty word")("")?;
     }
+    Ok((&s[1..], MOS::Reference("")))
+}
+
+fn qstr(s: &str) -> IResult<&str, MOS> {
+    alt((empty_word, quoted_str, word))(s)
+}
+
+fn qstr_tokenize(s: &str) -> IResult<&str, Vec<MOS>> {
+    let mut rest = s;
+    let mut tokens = Vec::new();
+    while !rest.is_empty() {
+        let (s, _) = multispace0(rest)?;
+        if s.is_empty() {
+            break;
+        }
+        let (s, mos) = qstr(s)?;
+        tokens.push(mos);
+        rest = s;
+    }
+    Ok(("", tokens))
+}
+
+#[test]
+pub fn qstr_test() {
+    let input = "foo bar _ \"foo bar\" baz \"escaped\\\"sl\\\\ash\" \"\"";
+    let got: Vec<MOS> = qstr_tokenize(input).unwrap().1;
+    let expected: Vec<MOS> = vec![
+        MOS::Reference("foo"),
+        MOS::Reference("bar"),
+        MOS::Reference(""),
+        MOS::Reference("foo bar"),
+        MOS::Reference("baz"),
+        MOS::Owned("escaped\"sl\\ash".into()),
+        MOS::Reference(""),
+    ];
+    assert_eq!(expected, got);
 }
 
 fn main() {
